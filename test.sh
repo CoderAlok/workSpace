f9dc629b3543cde131a1b35267488c753de083e1
coderAlok <alokdas4all@gmail.com>
2020-04-14 23:16:27 +0530

    Added a shell file that describles all the shell scripts

---
+++ test.sh
@@ -0,0 +1,111 @@
+# !/bin/bash
+# pwd
+# ls
+# ls -l -a
+
+
+
+
+
+
+
+
+# echo "What is your name?"
+# read PERSON
+# echo "Please mention your sex?"
+# read GENDER
+# echo "Where do you live?"
+# read PLACE
+
+# echo "Hello, myself "PERSON" from "PLACE". And i am "GENDER
+# echo "Hello, myself ".PERSON." from ".PLACE.". And i am ".GENDER
+
+
+
+
+
+
+
+# echo "---Calculator---"
+# echo "Enter the fisrt number : "
+# read val1
+# echo "Enter the Secound number : "
+# read val2
+# echo "Enter your choice : "
+# echo "1 : +"
+# echo "2 : -"
+# echo "3 : *"
+# echo "4 : /"
+# echo "5 : %"
+# echo "6 : x2"
+# echo "7 : x3"
+# read op
+
+
+
+
+
+# echo "Result : "+(val1+val2)
+# echo "Result : "(val1-val2)
+# echo "Result : "(val1*val2)
+# echo "Result : "(val1/val2)
+# echo "Result : "(val1%val2)
+# echo "Result : "(val1*val1)
+# echo "Result : "(val1*val1*val1)
+
+
+
+
+
+# NAME = "Zara Ali"
+# readonly NAME
+# NAME = "Quadiri"
+
+
+
+
+
+
+
+# This is for the command line 
+# ---------------------------------
+# echo "File Name : $0"
+# echo "First Parameter : $1"
+# echo "Secound Parameter : $2"
+# echo "Third Parameter : $3"
+# echo "Fourth Parameter : $4"
+# echo "Fifth Parameter : $5"
+# echo "Sixth Parameter : $6"
+# echo "Seventh Parameter : $7"
+# echo "Eighth Parameter : $8"
+# echo "Ninth Parameter : $9"
+# echo "Tenth Parameter : $10"
+# echo "Quoted Values : $@"
+# echo "Quoted Values : $*"
+# echo "Total number of Parameter : $#"
+
+
+
+
+
+
+# 1 Way
+cities=('Guwahati', 'Delhi', 'Amritsar', 'Kolkata', 'Pune', 'Bangalore')
+
+# 2 Way
+
+# cities[0]=Guwahati
+# cities[1]=Delhi
+# cities[2]=Amritsar
+# cities[3]=Kolkata
+# cities[4]=Pune
+# cities[5]=Bangalore
+# cities[6]=Nasik
+
+# echo cities[0]' is the capital of India.'
+echo "
+${cities[0]} is the capital of India.
+"
+
+# ------------Operators --------------
+# Arithmatic Operators 
